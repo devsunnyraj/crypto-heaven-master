83503f67837d6f5321e1020565ee7d5a768d3e30
Sunny Raj <hellotheresunnyhere@gmail.com>
2025-12-22 17:43:08 +0530

    Update page.tsx

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -35,7 +35,7 @@ async function Page(props: {
                     className='bg-dark-3 hover:bg-purple-600/20 text-purple-600 p-2 sm:bg-purple-600 sm:hover:bg-purple-700 sm:text-white sm:px-6 sm:py-2.5 rounded-lg text-sm sm:text-base font-semibold transition-colors flex items-center sm:gap-2 border border-purple-600/20 sm:border-0'
           // className='bg-black hover:bg-gray-900 text-white p-2 sm:bg-purple-600 sm:hover:bg-purple-700 sm:px-6 sm:py-2.5 rounded-lg text-sm sm:text-base font-semibold transition-colors flex items-center sm:gap-2 border border-gray-800 sm:border-0'
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <line x1="12" y1="5" x2="12" y2="19"></line>
             <line x1="5" y1="12" x2="19" y2="12"></line>
           </svg>
